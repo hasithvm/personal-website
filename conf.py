652d18dc8e1c038ff627760eba4f1ac482cdc853
Hasith Vidanamadura <hasith@vidanamadura.net>
2022-07-17 09:38:54 -0700

    Updated bio

--- conf.py
+++ conf.py
@@ -988,7 +988,6 @@ FEED_LINKS_APPEND_QUERY = False
 LICENSE = """
 <br>
 <div>
-This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
 <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png" /></a>
 </div>
 """
@@ -1003,7 +1002,7 @@ This work is licensed under a <a rel="license" href="http://creativecommons.org/
 # A small copyright notice for the page footer (in HTML).
 # (translatable)
 CONTENT_FOOTER = """
-Contents &copy; {date} by {author}. Last generated {build_time}{current_hash}. 
+&copy; {date} by {author}. Last generated {build_time}{current_hash}. 
 
 {license}"""
 
